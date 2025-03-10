65edc039c07c455a3476a61c49b8dbc04b114ec4
gabrielbarrera0305 <gabrielbarrera0305@gmail.com>
2025-03-10 11:13:04 -0400

    Add files via upload

---
+++ _font style=_vertical-align_ inherit;___font style=_vertical-align_ inherit;__principal.py
@@ -0,0 +1,23 @@
+a = 10
+b = 10
+c = 5
+d = 3
+f = 2
+suma = a + b
+print (suma)
+
+saludo = "hola buenos dias,"
+nombre = "mi nombre es gabriel barrera"
+edad = " tengo 17 años y "
+ciudad = "vivo en maracaibo"
+conversacion = saludo + nombre + edad + ciudad
+print (conversacion) 
+
+div = c / f
+print (div)
+
+ciencia = 18
+lengua = 17
+historia = 15
+calif = ciencia + lengua + historia / d
+print (calif)
