b17e29b231cfa13fd9d844180b869460408448fe
gabrielbarrera0305 <gabrielbarrera0305@gmail.com>
2025-03-10 22:40:42 -0400

    Add files via upload

---
+++ practicadeigualdad.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Ingresar un numero 1"))
+numero2 = int(input("Ingresar un numero 2"))
+
+if numero1 == numero2:
+    print ("los numeros son iguales")
+else: 
+    print ("los numeros no son iguales")
