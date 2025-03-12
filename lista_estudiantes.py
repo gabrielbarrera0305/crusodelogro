d44e987543302c3e1e0ab7def5f22d92e8ebf930
gabrielbarrera0305 <gabrielbarrera0305@gmail.com>
2025-03-12 09:36:15 -0400

    Add files via upload

---
+++ lista_estudiantes.py
@@ -0,0 +1,5 @@
+estudiantes =  ["jose miguel", "jose javier", "jose andres", "juan david", "genesis kai", "ana raquel", "ana paula", "alberto antonio", "hebert samuel", "alicia gabriela", "gabriel barrera", "santigo alfonso", "adan david", "angel david"]
+
+estudiantes.remove ("alicia gabriela")
+estudiantes.append ("daniel espitia")
+print (estudiantes)
