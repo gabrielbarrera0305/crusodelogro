bcb78052119d3ee2f59621c24522deeee0e08d80
gabrielbarrera0305 <gabrielbarrera0305@gmail.com>
2025-03-12 10:19:47 -0400

    Add files via upload

---
+++ praticade_dicionario.py
@@ -0,0 +1,6 @@
+diccionario = {"nombre":"gabriel","apellido":"barrera","edad":17,"peli favorita":"iron man 2"}
+
+print (diccionario["nombre"])
+print (diccionario["apellido"])
+print (diccionario["edad"])
+print (diccionario["peli favorita"])
